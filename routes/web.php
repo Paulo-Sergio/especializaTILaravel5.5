4c53fbf103da416391466709077e2eb5389361fa
Paulo França <paulofranca.contato@gmail.com>
2018-11-02 05:05:03 -0300

    implementando para editar perfil do usuario

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,9 @@ Route::group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'adm
   Route::get('/', 'AdminController@index')->name('admin.home');
 });
 
+Route::get('meu-perfil', 'Admin\UserController@profile')->name('profile')->middleware('auth');
+Route::post('atualizar-perfil', 'Admin\UserController@profileUpdate')->name('profile.update')->middleware('auth');
+
 Route::get('/', 'Site\SiteController@index')->name('home');
 
 Auth::routes();
